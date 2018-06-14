bb3e15bd44a51ff5329f8095d4ceb1238dc00761
reu2 <reu2@149-161-248-226.dhcp-bl.indiana.edu>
2018-06-14 17:37:40 -0400

    Testing: hope this works

---
+++ MamaGram/MamaGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MamaGram
+//
+//  Created by reu2 on 6/14/18.
+//  Copyright © 2018 reu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
