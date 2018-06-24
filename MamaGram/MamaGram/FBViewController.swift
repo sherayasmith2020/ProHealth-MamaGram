07df8ac59ae67f0ab086211afc1c602dbe6cf205
reu2 <reu2@149-161-248-226.dhcp-bl.indiana.edu>
2018-06-24 15:42:56 -0400

    Added multiple views

---
+++ MamaGram/MamaGram/FBViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FBViewController.swift
+//  MamaGram
+//
+//  Created by reu2 on 6/22/18.
+//  Copyright © 2018 reu. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class FBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
